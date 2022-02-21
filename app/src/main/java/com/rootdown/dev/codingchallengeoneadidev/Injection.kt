78ecddff5324d793cfea111671bc409dc80cf703
rootdownenergy <99987396+rootdownenergy@users.noreply.github.com>
2022-02-20 22:49:21 -0700

    Update Injection.kt

--- app/src/main/java/com/rootdown/dev/codingchallengeoneadidev/Injection.kt
+++ app/src/main/java/com/rootdown/dev/codingchallengeoneadidev/Injection.kt
@@ -7,11 +7,7 @@ import com.rootdown.dev.codingchallengeoneadidev.data.feature_character_explorer
 import com.rootdown.dev.codingchallengeoneadidev.data.feature_character_explorer.repo.CharacterImpl
 import com.rootdown.dev.codingchallengeoneadidev.viewmodel.ViewModelFactory
 
-/**
- * Class that handles object creation.
- * Like this, objects can be passed as parameters in the constructors and then replaced for
- * testing, where needed.
- */
+
 object Injection {
 
     private fun provideBreakingBadAppRepository(context: Context): CharacterImpl {
