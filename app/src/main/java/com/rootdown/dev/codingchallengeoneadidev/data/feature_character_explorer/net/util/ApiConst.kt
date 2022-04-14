ac4dfda22c7c7a0c701d7382211184c5c44b69e7
rootdownenergy <99987396+rootdownenergy@users.noreply.github.com>
2022-04-14 14:55:05 -0600

    Delete ApiConst.kt

--- app/src/main/java/com/rootdown/dev/codingchallengeoneadidev/data/feature_character_explorer/net/util/ApiConst.kt
+++
@@ -1,4 +0,0 @@
-package com.rootdown.dev.codingchallengeoneadidev.data.feature_character_explorer.net.util
-
-const val BASE_URL = "https://breakingbadapi.com"
-const val API_CHARACTERS = "/api/characters"
