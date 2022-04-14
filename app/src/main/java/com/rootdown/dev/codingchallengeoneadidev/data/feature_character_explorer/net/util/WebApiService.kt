c6173226f9577043d85ed2e44960ee572a7be9e2
rootdownenergy <99987396+rootdownenergy@users.noreply.github.com>
2022-04-14 14:55:14 -0600

    Delete WebApiService.kt

--- app/src/main/java/com/rootdown/dev/codingchallengeoneadidev/data/feature_character_explorer/net/util/WebApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.rootdown.dev.codingchallengeoneadidev.data.feature_character_explorer.net.util
-
-import com.rootdown.dev.codingchallengeoneadidev.data.feature_character_explorer.db.BreakingBadChar
-import retrofit2.http.GET
-
-interface WebApiService {
-    @GET(API_CHARACTERS)
-    fun getBreakingBadChars(): List<BreakingBadChar>
-}
